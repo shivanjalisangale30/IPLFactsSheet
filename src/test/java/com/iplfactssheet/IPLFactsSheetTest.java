021b983cc3fadcd3536e1b620fb90d3394015370
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-12-14 14:33:00 +0530

    Added test case for IPLRun of given file type is not supported should handle exception

--- src/test/java/com/iplfactssheet/IPLFactsSheetTest.java
+++ src/test/java/com/iplfactssheet/IPLFactsSheetTest.java
@@ -8,6 +8,8 @@ public class IPLFactsSheetTest {
     String IPL_BATTING_TEAM = "/home/admin1/Desktop/IPL/src/test/resources/IPL2019FactsheetMostRuns.csv";
     String EMPTY_FILE = "/home/admin1/Desktop/IPL/src/test/resources/EmptyFile.csv";
     String DELIMETER_BATTING_FILE = "/home/admin1/Desktop/IPL/src/test/resources/Delimeter_Issue_Runs.csv";
+    String FILE_TYPE_NOT_SUPPORTED = "/home/admin1/Desktop/IPL/src/test/resources/IPL2019FactsheetMostRuns.json";
+
 
     @Test
     public void givenIPLFactsSheetOfMostRunsFile_WhenProper_ShouldGiveCorrectCount() {
@@ -46,6 +48,16 @@ public class IPLFactsSheetTest {
             iplFactAnalyserTeam.loadBattingTeamData(DELIMETER_BATTING_FILE);
         } catch (IPLFactAnalyserException e) {
             Assert.assertEquals(IPLFactAnalyserException.ExceptionType.SOME_FILE_ISSUE, e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLFactsSheetOfMostRunsFile_WhenGivenFileTypeIsNotSupported_ShouldHandleException() {
+        IPLFactAnalyserTeam iplFactAnalyserTeam = new IPLFactAnalyserTeam();
+        try {
+            iplFactAnalyserTeam.loadBattingTeamData(FILE_TYPE_NOT_SUPPORTED);
+        } catch (IPLFactAnalyserException e) {
+            Assert.assertEquals(IPLFactAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
     }
 }
